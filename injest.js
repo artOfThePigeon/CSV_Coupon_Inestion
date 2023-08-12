a6a489bfe51ebf485fc525f6f47ed778f319cb25
David Lev Wilson <42543340+artOfThePigeon@users.noreply.github.com>
2023-08-12 14:46:01 -0700

    created addRow function and confirmed unique objects are created from csv rows

--- injest.js
+++ injest.js
@@ -1,3 +1,6 @@
+function balls(obj) {
+	return obj
+}
 function previewFile() {
 	const content = document.querySelector(".content");
 	const [file] = document.querySelector("input[type=file]").files;
@@ -9,16 +12,25 @@ function previewFile() {
 
 	reader.onload = () => {	
 		
-		let rowObj = {}
 		let values = []
+		let csvList = []
+		let csvRow = {}
 		let csv = reader.result.split('\n');
 		let keys = csv.shift().split(',');
 		for (let row = 0; row < csv.length; row++) {
 			values = csv[row].split(',');
-			for (let i = 0; i < keys.length; i ++) {
-				rowObj[keys[i]] = values[i];
+			csvRow = addRow(keys, values)
+			csvList.push(csvRow)
+		}
+		console.log(csvList)
+	
+		function addRow(keys, values) {
+			let rowObj = {}
+			for (let i = 0; i < values.length; i++) {
+				rowObj[keys[i]] = values[i]
 			}
-			console.log(rowObj)
+			return rowObj;
+
 		}
 	}
 }
