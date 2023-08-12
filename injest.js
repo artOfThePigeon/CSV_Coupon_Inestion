868f866b897c1052f5a73689b845e65a1efec581
David Lev Wilson <42543340+artOfThePigeon@users.noreply.github.com>
2023-08-12 01:34:35 -0700

    fixed issue with extracting CSV field into the object.

--- injest.js
+++ injest.js
@@ -1,7 +1,4 @@
-let csvList = []
-
 function previewFile() {
-	let csvRow = {};
 	const content = document.querySelector(".content");
 	const [file] = document.querySelector("input[type=file]").files;
 	const reader = new FileReader();
@@ -11,22 +8,17 @@ function previewFile() {
 	}
 
 	reader.onload = () => {	
+		
 		let rowObj = {}
 		let values = []
 		let csv = reader.result.split('\n');
 		let keys = csv.shift().split(',');
-		for (let row of csv) {
-			values = row.split(',');
+		for (let row = 0; row < csv.length; row++) {
+			values = csv[row].split(',');
 			for (let i = 0; i < keys.length; i ++) {
 				rowObj[keys[i]] = values[i];
-				Object.assign(rowObj, rowObj)
-				console.log(rowObj)
 			}
-			csvList.push(rowObj);
+			console.log(rowObj)
 		}
-	
-		console.log(csv);
-		console.log(rowObj);
-		console.log(csvList);
 	}
 }
