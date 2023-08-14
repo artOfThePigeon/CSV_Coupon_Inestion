addc18b64ef548c4254fe51c14bafac70c70ec52
David Lev Wilson <42543340+artOfThePigeon@users.noreply.github.com>
2023-08-14 11:15:49 -0700

    broke out code into 3 distinct functions and optimized submit button

--- injest.js
+++ injest.js
@@ -1,35 +1,33 @@
 function previewFile() {
-	const content = document.querySelector(".content");
 	const [file] = document.querySelector("input[type=file]").files;
 	const reader = new FileReader();
-
-	if (file) {
-		reader.readAsText(file);
-	}
-
+	if (file) reader.readAsText(file);
+	
 	reader.onload = () => {
-		console.log(csvToList())
-	}	
-		function csvToList() {
-			let csvList = [];
-			let values = [];
-			let lineItem = {};
-			let rows = reader.result.split('\n');
-			let keys = rows.shift().split(',');
-			for (let row = 0; row < rows.length; row++) {
-				values = rows[row].split(',');
-				lineItem = addRow(keys, values)
-				csvList.push(lineItem)
-			}
-			return csvList
-		}
-		function addRow(keys, values) {
-			let rowObj = {}
-			for (let i = 0; i < values.length; i++) {
-				rowObj[keys[i]] = values[i]
-			}
-			return rowObj;
-
-		}
+		console.log(csvToList(reader))
 	}
+}
+
+function csvToList(reader) {
+	let csvList = [];
+	let values = [];
+	let lineItem = {};
+	let rows = reader.result.split('\n');
+	let keys = rows.shift().split(',');
+	for (let row = 0; row < rows.length; row++) {
+		values = rows[row].split(',');
+		lineItem = addRow(keys, values)
+		csvList.push(lineItem)
+	}
+	return csvList
+}
+		
+function addRow(keys, values) {
+	let rowObj = {}
+	for (let i = 0; i < values.length; i++) {
+		rowObj[keys[i]] = values[i]
+	}
+	return rowObj;
+}
+		
 
